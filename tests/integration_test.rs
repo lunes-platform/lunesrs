fc969ca88ccf0da8dbce9120ea5ee09ddb9ccf1b
Lucas Oliveira <lucas.oliveira>
2022-01-03 03:19:39 +0000

    fixed: integration test
    need add `lib` in `crate-type` for run integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,8 +1,16 @@
 use lunes_wasm;
 
 #[test]
-fn total_suplay_of_lunes() {
-    let response = lunes_wasm::total_suplay_mainnet();
+fn total_suplay_of_lunes_mainnet() {
+    let response = lunes_wasm::total_supply_mainnet();
     let result = 150_728_537.61498705;
+    assert_eq!(response, result);
+}
+
+
+#[test]
+fn total_suplay_of_lunes_testnet() {
+    let response = lunes_wasm::total_supply_testnet();
+    let result = 800_100_000.0;
     assert_eq!(response, result);
 }
