697678b109417171b98009ad4cc6f2cb66bc1e01
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-03-28 22:44:25 -0300

    updated: docs

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,58 @@
 //! For the user guide and further documentation, please read
 //! [Telescope](https:blockchain.lunes.io/telescope)
 //!
+//! ## 🏗 Archtecture
+//!
+//! ```rust
+//! mod utils {
+//!     pub mod random {
+//!         fn randomTripleNumber -> Vec<u32>
+//!         fn randomBytes(usize) -> Vec<u32>
+//!     }
+//!     pub mod strings {
+//!         fn stringToB58
+//!         fn stringToVecu32
+//!         fn vecu32ToString
+//!         fn stringToUint32Array
+//!         fn uint32ArrayToString
+//!     }
+//!     pub mod base58 {
+//!         fn b58ToVec
+//!         fn vecToB58
+//!     }
+//!     pub mod hexadecimals {
+//!         fn fromStrHex
+//!         fn hexToB58
+//!         fn vecu32ToHex
+//!     }
+//!     pub mod vectors {
+//!         fn toVecu32(Vec<u8>) -> Vec<u32>
+//!         fn toVecu8(Vec<u32>) -> Vec<u8>
+//!     }
+//!     mod serialize {
+//!         fn serializeString(String) -> Vec<u8>
+//!         fn serializeUInteger(u64) -> Vec<u8>
+//!     }
+//! }
+//! mod account {
+//!     mod signature {
+//!         fn validateAddress
+//!         fn validateSignature
+//!         fn decode_message
+//!         fn fullSignature
+//!         fn fastSignature
+//!     }
+//!     mod crypto {
+//!         fn toPrivateKeyHex
+//!         fn toPublicKeyHex
+//!         fn toAddressHex
+//!         fn hiddenSeed
+//!         fn keypair_new
+//!         fn keypair_privateKey
+//!         fn keypair_publiKey
+//!     }
+//! }
+//!
 
 /// Functions for wallets for Lunes Blockchain
 pub mod account;
