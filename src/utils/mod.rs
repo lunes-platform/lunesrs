c058808ba858651f5fa8806e30e3794047e5c8e1
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-03-28 22:11:19 -0300

    added: utils/vectors

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,7 @@
 /// Functions for get vetor of random numbers
 pub mod random;
+/// convert vectors between u32 and u8
+pub mod vectors;
 
 pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
